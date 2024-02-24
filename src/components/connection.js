c7e07baa1869f57a159e0a3ce4fa1a10b91921fa
Alexito-Hub <alessandrovillogas@outlook.es>
2024-02-24 15:01:04 -0500

    update package

--- src/components/connection.js
+++ src/components/connection.js
@@ -27,22 +27,21 @@ const {
 
 const fs = require("fs")
 const pino = require("pino")
-const cli = require("cli-color");
 const { exec } = require("child_process")
 
 
-const utils = require("../../lib/utils")
-const font = require("../../lib/font")
+// const utils = require("../../lib/utils")
+// const font = require("../../lib/font")
 
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+// const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 exports.connect = async (start) => {
     const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" })})
-    const spinner = font.getGlobalSpinner();
+    // const spinner = font.getGlobalSpinner();
     const sessionExists = await fs.promises.access('./auth/session').then(() => true).catch(() => false);
 
-    await utils.statusSession(spinner, sessionExists);
-    await sleep(4000)
+    // await utils.statusSession(spinner, sessionExists);
+    // await sleep(4000)
     const { state, saveCreds } = await useMultiFileAuthState('./auth/session')
     const sock = WAConnection({
         logger : pino({ level : "silent" }),
@@ -70,9 +69,10 @@ exports.connect = async (start) => {
                 })
             }
         } else if (connection === "open") {
-            const copyright = font.copyright();
-            const progress = font.progressBar(5000);
-            console.log(progress, copyright)
+            // const copyright = font.copyright();
+            // const progress = font.progressBar(5000);
+            // console.log(progress, copyright)
+            console.log("oppened connection")
         }
     })
     
