c282df59e6aebd06af6cc69050542ded8215c0ae
Ziooo <103476365+Alexito-Hub@users.noreply.github.com>
2024-02-26 09:12:33 -0500

    Actualizar _antilink.js

--- middlewares/_antilink.js
+++ middlewares/_antilink.js
@@ -1,28 +1,28 @@
 const GroupConfig = require('../models/_antilink');
 
-async function antilinkMiddleware(sock, m, isMe, isAdmin, isBotAdmin) {
-    try {
-        if (!m.isGroup) return; // Verifica si el mensaje es de un grupo
-
-        const groupId = m.chat;    
-        const groupConfig = await GroupConfig.findOne({ groupId });
-
-        if (!groupConfig || !groupConfig.antilink) return; // Verifica si el antienlace está habilitado para el grupo
-
-        const containsLink = /(http|https):\/\/\S+|www\.\S+|\S+\.\S+/i.test(m.body);
-
-        if (containsLink) {
-      
-                if (!isAdmin && !isMe) {
-                    await sock.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-                    v.reply("Enlaces no permitidos. Has sido eliminado.");
-                }
-
-        }
-        
-    } catch (error) {
-        console.error('Error en el middleware anti-enlace:', error);
+async function antiLinkMiddleware(sock, m, next, isAdmin) {
+    if (!m.isGroup) {
+        next();
+        return;
     }
+
+    const groupId = m.chat;
+    const groupConfig = await GroupConfig.findOne({ groupId });
+
+    // Verificar si el usuario no es un administrador del grupo
+    
+    if (!isAdmin) {
+        next();
+        return;
+    }
+
+    if (groupConfig && !isMe && groupConfig.antiLinkEnabled && /https?:\/\/\S+/.test(m.body)) {
+        await sock.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+        await sock.sendMessage(m.chat, { text: 'Enlaces no permitidos. Has sido eliminado.' });
+        return;
+    }
+
+    next();
 }
 
 module.exports = antilinkMiddleware;
